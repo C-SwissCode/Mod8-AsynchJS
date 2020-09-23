2f84eb2038ae021fc14268b31995fbd501053819
Caleb <cswiss12@protonmail.com>
2020-09-22 23:00:06 -0500

    AJAX calls with Fetch and Promises

--- script.js
+++ script.js
@@ -16,29 +16,29 @@
  * Promises
  */
 
- const getIDs = new Promise((resolve, reject) => {
-   setTimeout(() => {
-     resolve([523, 883, 432, 974]);
-   }, 1500);
- });
+//  const getIDs = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//      resolve([523, 883, 432, 974]);
+//    }, 1500);
+//  });
 
- const getRecipe = recID => {
-   return new Promise((resolve, reject) => {
-    setTimeout(ID => {
-      const recipe = {title: 'Fresh Tomato Pasta', publisher: 'Jonas'};
-      resolve(`${ID}: ${recipe.title}`);
-    }, 1500, recID);
-   });
- };
+//  const getRecipe = recID => {
+//    return new Promise((resolve, reject) => {
+//     setTimeout(ID => {
+//       const recipe = {title: 'Fresh Tomato Pasta', publisher: 'Jonas'};
+//       resolve(`${ID}: ${recipe.title}`);
+//     }, 1500, recID);
+//    });
+//  };
 
- const getRelated = publisher => {
-   return new Promise((resolve, reject) => {
-     setTimeout(pub => {
-       const recipe = {title: 'Itallian Pizza', publisher: 'Jonas'};
-       resolve(`${pub}: ${recipe.title}`);
-     }, 1500, publisher);
-   });
- };
+//  const getRelated = publisher => {
+//    return new Promise((resolve, reject) => {
+//      setTimeout(pub => {
+//        const recipe = {title: 'Itallian Pizza', publisher: 'Jonas'};
+//        resolve(`${pub}: ${recipe.title}`);
+//      }, 1500, publisher);
+//    });
+//  };
 
 //  getIDs.then(IDs => {
 //    console.log(IDs)
@@ -56,15 +56,37 @@
  * Async Await
  */
 
-async function getRecipesAW() {
-  const IDs = await getIDs;
-  console.log(IDs);
-  const recipe = await getRecipe(IDs[2]);
-  console.log(recipe);
-  const publisher = await getRelated('Caleb');
-  console.log(publisher);
+// async function getRecipesAW() {
+//   const IDs = await getIDs;
+//   console.log(IDs);
+//   const recipe = await getRecipe(IDs[2]);
+//   console.log(recipe);
+//   const publisher = await getRelated('Caleb');
+//   console.log(publisher);
 
-  return recipe;
+//   return recipe;
+// };
+
+// getRecipesAW().then(result => console.log(`${result} is the best recipe ever!!`));
+
+/*****************
+ * AJAX calls with Fetch and Promises
+ */
+
+function getWeather(woeid) {
+  fetch(`https://cors-anywhere.herokuapp.com/www.metaweather.com/api/location/${woeid}`)
+    .then(result => {
+      console.log(result);
+      return result.json();
+    })
+    .then(data => {
+      console.log(data);
+      const today = data.consolidated_weather[0];
+      let tempHighF = today.max_temp * (9 / 5) + 32;
+      let tempLowF = today.min_temp * (9 / 5) + 32;
+      console.log(`Today in ${data.title} the high will be ${tempHighF} and the low is ${tempLowF}, I can't wait for fall!`);
+    });
 };
 
-getRecipesAW().then(result => console.log(`${result} is the best recipe ever!!`));
+getWeather(2487956);
+getWeather(44418);
